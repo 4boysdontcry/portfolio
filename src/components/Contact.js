4c8d84a2fae3ddb715c975efd27ec4d8275517e2
4boysdontcry <hgy02166@gmail.com>
2021-07-01 23:13:48 +0900

    contact 애니메이션 완료

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import styled, { css } from 'styled-components'
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import { Col, Image, Card } from 'react-bootstrap'
+import WOW from 'wowjs'
 
 
 /* ****************** styled ****************** */
@@ -12,14 +11,19 @@ import '../css/contact.css'
 /* ****************** component ****************** */
 
 const Contact = () => {
+  
+  useEffect(() => {
+    new WOW.WOW().init();
+  }, []);
+
   return (
     <div className="contact-wrapper">
       <div className="contact-wrap">
         <h3 className="title">CONTACT</h3>
         <div className="call-wrapper">
-          <div className="call"><a href="tel:010-8724-7111">📞 +82(0)10-8724-7111</a></div>
-          <div className="call"><a href="mailto:hgy02166@gmail.com">📬 hgy02166@gmail.com</a></div>
-          <div className="call"><a href="https://github.com/4boysdontcry" target="_blank"><i className="fa fa-github" /> github.com/4boysdontcry</a></div>
+          <div className="call wow animate__backInLeft"><a href="tel:010-8724-7111">📞 +82(0)10-8724-7111</a></div>
+          <div className="call wow animate__backInRight"><a href="mailto:hgy02166@gmail.com">📬 hgy02166@gmail.com</a></div>
+          <div className="call wow animate__backInLeft"><a href="https://github.com/4boysdontcry" target="_blank"><i className="fa fa-github" /> github.com/4boysdontcry</a></div>
         </div>
       </div>
     </div>
